c212550559c01ba132d6b383bbd3067c255542ed
y145450590 <cetinkayaaemrah@gmail.com>
2018-01-14 20:09:49 +0000

    first web project

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  def create
+    @post= Post.find(params[:post_id])
+    @comment=@post.comments.build(params.require(:comment).permit(:text))
+    @comment.save
+    
+    redirect_to @post
+  end
+  def myCv
+  end
+  def destroy
+    @comment=Comment.find(params[:id])
+    @comment.destroy
+    
+    redirect_to @comment.post
+  end
+  
+end
---
+++ app/controllers/cv_controller.rb
@@ -0,0 +1,4 @@
+class CvController < ApplicationController
+    def myCv
+    end
+end
